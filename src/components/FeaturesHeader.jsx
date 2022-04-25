50e064529500f5e937f17cc8f5e3c73290f65d0d
mpabarca <mpabarca@uc.cl>
2022-04-25 10:21:30 -0400

    Add fontawesome icon to project

--- src/components/FeaturesHeader.jsx
+++ src/components/FeaturesHeader.jsx
@@ -1,13 +1,15 @@
+import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 function FeaturesHeader() {
   return (
     <nav className='features'>
       <a href='/'>ES</a>
-      <span>/</span>
       <a href='/'>EN</a>
-      <a href='/'>DARK</a>
-      <a href='/'>LIGHT</a>
+      <a href='/'>
+        <FontAwesomeIcon icon={faCircleHalfStroke} />
+      </a>
     </nav>
   );
 }
--- src/components/SocialMedias.jsx
+++ src/components/SocialMedias.jsx
@@ -1,12 +1,27 @@
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 function SocialMedias() {
   return (
     <nav className='social-medias'>
-      <a href='/'>I</a>
-      <a href='/'>LK</a>
-      <a href='/'>GH</a>
-      <a href='/'>C</a>
+      <a href='/'>
+        <FontAwesomeIcon icon={faInstagram} />
+      </a>
+      <a href='/'>
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a href='/'>
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a href='/'>
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
     </nav>
   );
 }
