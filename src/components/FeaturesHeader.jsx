95cc0b1df366075da7a2c01030ffe8d358f4ccc0
mpabarca <mpabarca@uc.cl>
2022-04-29 21:44:36 -0400

    Create link.json to save all link data

--- src/components/FeaturesHeader.jsx
+++ src/components/FeaturesHeader.jsx
@@ -1,16 +1,14 @@
 import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { changeLanguage } from '../redux/language';
 
 function FeaturesHeader() {
-  const { languageSelected } = useSelector((state) => state.language);
   const dispatch = useDispatch();
 
   return (
     <nav className='features'>
-      <h1>{languageSelected}</h1>
       <button
         type='button'
         value='spanish'
--- src/components/SocialMedias.jsx
+++ src/components/SocialMedias.jsx
@@ -6,20 +6,21 @@ import {
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import data from '../data/link.json';
 
 function SocialMedias() {
   return (
     <nav className='social-medias'>
-      <a href='/'>
+      <a target='_blank' href={data.instagram} rel='noreferrer'>
         <FontAwesomeIcon icon={faInstagram} />
       </a>
-      <a href='/'>
+      <a target='_blank' href={data.linkedIn} rel='noreferrer'>
         <FontAwesomeIcon icon={faLinkedin} />
       </a>
-      <a href='/'>
+      <a target='_blank' href={data.github} rel='noreferrer'>
         <FontAwesomeIcon icon={faGithub} />
       </a>
-      <a href='/'>
+      <a target='_blank' href={`mailto:${data.email}`} rel='noreferrer'>
         <FontAwesomeIcon icon={faEnvelope} />
       </a>
     </nav>
